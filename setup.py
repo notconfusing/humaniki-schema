24c32cc059052fd76fc8d7fa93a9a46ef4337ff3
Max Klein <isalix@gmail.com>
2020-10-09 10:59:28 -0700

    generate sitelink example data

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='humaniki-schema',
+    version='0.1',
+    packages=['humaniki_schema'],
+    url='',
+    license='',
+    author='notconfusing',
+    author_email='',
+    description=''
+)
