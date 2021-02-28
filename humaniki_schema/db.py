548fa9817a07933aa201c4d78a24b244ea6ffd62
Max <max@notconfusing.com>
2021-02-28 13:08:31 -0800

    add pool recycle and pre ping

--- humaniki_schema/db.py
+++ humaniki_schema/db.py
@@ -3,13 +3,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 engine = create_engine("mysql://{user}:{password}@{host}/{database}?charset=utf8".format(
-    host = os.environ['HUMANIKI_MYSQL_HOST'],
-    user = os.environ['HUMANIKI_MYSQL_USER'],
-    password = os.environ['HUMANIKI_MYSQL_PASS'],
-    database = os.environ['HUMANIKI_MYSQL_DB']),
+    host=os.environ['HUMANIKI_MYSQL_HOST'],
+    user=os.environ['HUMANIKI_MYSQL_USER'],
+    password=os.environ['HUMANIKI_MYSQL_PASS'],
+    database=os.environ['HUMANIKI_MYSQL_DB']),
     pool_size=10,
     max_overflow=20,
-    )
+    pool_recycle=3600,
+    pool_pre_ping=True
+)
 
 # Base.metadata.bind = db_engine
 session_factory = sessionmaker(bind=engine)
