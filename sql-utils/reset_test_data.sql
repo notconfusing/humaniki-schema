bb2939293eb1f1a7ad05cb4972e92f949aef999a
Max <max@notconfusing.com>
2021-06-14 10:17:27 -0700

    add sql utils

---
+++ sql-utils/reset_test_data.sql
@@ -0,0 +1,18 @@
+truncate human_country;
+truncate human_occupation;
+truncate human_property;
+truncate human_sitelink;
+truncate label;
+truncate human;
+
+truncate job;
+truncate metric;
+truncate metric_aggregations_j;
+truncate metric_aggregations_n;
+truncate metric_properties_j;
+truncate metric_properties_n;
+truncate metric_coverage;
+
+delete
+from fill
+where date = '2020-11-30';
---
+++ sql-utils/set jobs to 1.sql
@@ -0,0 +1,3 @@
+update job
+set job_state=1, errors=null
+where true;
